8baefca0c4dbff625518dd5912f795024d15fdbb
Maria <you@example.com>
2023-11-23 22:56:31 -0400

    Google fonts

---
+++ src/fonts.ts
@@ -0,0 +1,11 @@
+import { Raleway, Open_Sans } from 'next/font/google'
+ 
+export const raleway = Raleway({
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
+export const open_Sans = Open_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+})

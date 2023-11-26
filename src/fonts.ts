804a3e3b1e49ea3ea920262eae9a35a61d470808
Maria <you@example.com>
2023-11-26 15:32:12 -0400

    Fonts variables

--- src/fonts.ts
+++ src/fonts.ts
@@ -3,9 +3,11 @@ import { Raleway, Open_Sans } from 'next/font/google'
 export const raleway = Raleway({
   subsets: ['latin'],
   display: 'swap',
+  variable: '--font-raleway',
 })
  
-export const open_Sans = Open_Sans({
+export const open_sans = Open_Sans({
   subsets: ['latin'],
   display: 'swap',
+  variable: '--font-opensans',
 })
